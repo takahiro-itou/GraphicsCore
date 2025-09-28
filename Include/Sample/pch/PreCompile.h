31b9e485ff67e45798295169053226d115ea696c
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-09-28 12:40:15 +0900

    空のヘッダファイルを新規作成

---
+++ Include/Sample/pch/PreCompile.h
@@ -0,0 +1,24 @@
+﻿//  -*-  coding: utf-8-with-signature;  mode: c++  -*-  //
+/*************************************************************************
+**                                                                      **
+**                      ---  Library Project.  ---                      **
+**                                                                      **
+**          Copyright (C), 2016-2024, Takahiro Itou                     **
+**          All Rights Reserved.                                        **
+**                                                                      **
+**          License: (See COPYING or LICENSE files)                     **
+**          GNU Affero General Public License (AGPL) version 3,         **
+**          or (at your option) any later version.                      **
+**                                                                      **
+*************************************************************************/
+
+/**
+**      Pre-Compiled Header File.
+**
+**      @file       pch/PreCompile.h
+**/
+
+#if !defined( SAMPLE_PCH_INCLUDED_PRE_COMPILE_H )
+#    define   SAMPLE_PCH_INCLUDED_PRE_COMPILE_H
+
+#endif
