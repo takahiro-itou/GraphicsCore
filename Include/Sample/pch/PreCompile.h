9cc7b1791c4ebe908eb79bfb1044326b98e4ed90
Takahiro Itou <6907757+takahiro-itou@users.noreply.github.com>
2025-10-04 21:18:45 +0900

    ヘッダのインクルードを追加

--- Include/Sample/pch/PreCompile.h
+++ Include/Sample/pch/PreCompile.h
@@ -21,4 +21,14 @@
 #if !defined( SAMPLE_PCH_INCLUDED_PRE_COMPILE_H )
 #    define   SAMPLE_PCH_INCLUDED_PRE_COMPILE_H
 
+#if !defined( SAMPLE_SYS_INCLUDED_STDDEF_H )
+#    include    <stddef.h>
+#    define   SAMPLE_SYS_INCLUDED_STDDEF_H
+#endif
+
+#if !defined( SAMPLE_SYS_INCLUDED_STDINT_H )
+#    include    <stdint.h>
+#    define   SAMPLE_SYS_INCLUDED_STDINT_H
+#endif
+
 #endif
